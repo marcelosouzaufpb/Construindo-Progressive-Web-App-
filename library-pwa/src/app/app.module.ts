47920b81fdcb8bfbf9e216b86e842776708281cc
Marcelo <marcelo.souza@dcx.ufpb.br>
2020-06-07 16:19:18 -0300

    creating menu and book entity with your first step creating

--- library-pwa/src/app/app.module.ts
+++ library-pwa/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { MenuComponent } from './components/menu/menu.component';
+import { CreateBookComponent } from './components/book/create-book/create-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CreateBookComponent
   ],
   imports: [
     BrowserModule,
